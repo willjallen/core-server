17bb5110b4e01393f4c71209fc5747777977c77a
Will Allen <wallen2268@gmail.com>
2020-08-11 02:47:33 -0500

    Allowed multiple actions to message service

--- src/hooks/process-message.js
+++ src/hooks/process-message.js
@@ -13,10 +13,12 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     // The actual message text
     // Make sure that messages are no longer than 400 characters
     const { text } = data;
+    const messageId = data.id;
 
     // Update the original data (so that people can't submit additional stuff)
     context.data = {
       text,
+      messageId,
       // Set the user id
       userId: user._id,
       // Add the current date
